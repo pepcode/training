76bc0b9336f6e370521a9fd40f9a060d5f946f49
praveenmanoharan <praveen@localhost>
2021-08-22 20:32:33 -0700

    update steam comments

--- src/main/java/pep/training/streams/StreamExample.java
+++ src/main/java/pep/training/streams/StreamExample.java
@@ -3,10 +3,20 @@ package pep.training.streams;
 import java.util.stream.IntStream;
 
 /**
- * more efficeient
- * heavy use of lambda expressions
- * parallel streams  makes it very easy for multithreading
  * 
+ * Stream Advantages - 
+ * 1. more efficeient
+ * 2. heavy use of lambda expressions
+ * 3. parallel streams  makes it very easy for multithreading
+ * 4. Declarative style of coding
+ * 
+ *  *****************			*****************			*****************
+ *  *				*			*				*			*				*
+ *  *    source		*************   Interim Ops ************* Terminal Ops	*
+ *  *				*			*				*			*				*
+ *  *****************			*****************			*****************
+ *  
+ *  
  * source (collection, list, array  ) ->  
  * 		(zero or more Intermediate operations)filter or sort or map or skip or distinct or find -> 
  * 			collect or reduce or Foreach or max or min or count (terminal operations)
